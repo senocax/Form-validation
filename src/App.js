59370d1cf5bca4a4212a1ebc51d9f1ac415c5553
senocax <facebookshop@hotmail.com>
2020-09-11 14:07:24 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import UserForm from "./Formu";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <UserForm />
+    </div>
+  );
+}
