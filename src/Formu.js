5704fadba0f97e6f3fca5e98e748bb0a23c821cb
senocax <facebookshop@hotmail.com>
2020-09-14 08:58:19 -0300

    add email validation

--- src/Formu.js
+++ src/Formu.js
@@ -1,28 +1,61 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 
 const UserForm = () => {
-  return (
-    <Form>
-      <Form.Group controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    var emailValid = false;
+    if (email.length == 0) {
+      setEmailError("Email is required");
+    } else if (email.length < 6) {
+      setEmailError("Email should be minimum 6 characters");
+    } else if (email.indexOf(" ") >= 0) {
+      setEmailError("Email cannot contain spaces");
+    } else {
+      setEmailError("");
+      emailValid = true;
+    }
+  };
+
+  return (
+    <div>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onChange={(e) => setEmail(event.target.value)}
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(event.target.value)}
+          />
+        </Form.Group>
+        <Form.Group controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+      Email entered: {email}
+      <br />
+      Password entered: {password}
+    </div>
   );
 };
 
